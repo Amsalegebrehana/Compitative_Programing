ea0f86734da96442f4f95ca6e7eb7dc5e44d70cb
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-10-13 01:22:04 +0300

    two sum

--- Day4/TwoSum.py
+++ Day4/TwoSum.py
@@ -1,1 +1,11 @@
 #leetcode two sum solution
+def twoSum():
+    nums = [3,2,4] 
+    target = 6
+    for i in range(len(nums)):
+                for j in range(len(nums)):
+                        
+                        if nums[i] + nums[j] == target and i != j:
+                            return i,j
+
+print(twoSum())
