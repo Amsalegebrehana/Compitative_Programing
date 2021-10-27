e8d709fc1ca1cff26f5f01e24b91fa769054b9a6
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-10-27 15:41:10 +0300

    sort square numbers

---
+++ Day12/squaresofsortedarr.py
@@ -0,0 +1,17 @@
+#leetcode squares of sorted array 977
+
+def sortsqr(nums):
+    n = []
+    nums = [i**2 for i in nums]
+
+    c = 0
+    for i in range (c, len(nums)):
+        num = min(nums)
+       
+        n.append(num)
+        nums.remove(num)
+       
+        # nums[c]=num
+  
+    return n
+print(sortsqr([1,3,1,2]))
