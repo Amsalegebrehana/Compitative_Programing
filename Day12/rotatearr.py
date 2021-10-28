b71a459d8306bbcbb0e579ee5a7e895847f2a999
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-10-28 16:33:02 +0300

    2

--- Day12/rotatearr.py
+++ Day12/rotatearr.py
@@ -1,21 +1,17 @@
 #leetcode rotate arr 189 solution 
 
 def rotateArr(nums,k):
-    n = []
-    print(nums[len(nums)-k])
-    for i in range(len(nums)-k, len(nums)):
-        n.append(nums[i])
-        print(nums[i])
-    # for i in range(k, 0,-1):
-    #     num = nums[-1]
-    #     nums.remove(num)
-    #     nums.insert(0, num)
-
-    h = nums[k+1:] + nums[:k+1]
-    print(n)
-    print(h)
+    
+    k = k % len(nums)
+    while k > 0:
+        
+        num = nums.pop()
+        nums.insert(0, num)
+        k -=1
     return nums
+    
 
 
-print(rotateArr([1,2,3,4,5,6,7],3))
+print(rotateArr([1,2],3))
+print(rotateArr([-1,-100,3,99], 2))
 
