ec72b810527a9172edb0b34be52592b44a5b796c
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-11-01 11:03:12 +0300

    merge sort

--- Day11/dividetwointeger.py
+++ Day11/dividetwointeger.py
@@ -1,25 +1,29 @@
-
+# import math
 #leetcode 29 divide to integer solution
 def divide( dividend, divisor):
-        if str(dividend)[0] == '-' and str(divisor)[0] != '-':
-            dividend = int(str(dividend)[1:])
-            result = '-'
-        elif str(divisor)[0] == '-'  and str(dividend)[0] != '-':
-            divisor = int(str(divisor)[1:])
-            result = '-'
-        elif str(dividend)[0] == '-' and str(divisor)[0] == '-':
-            divisor = int(str(divisor)[1:])
-             
-            dividend = int(str(dividend)[1:])
-            result =''
+        div = 0
+        if dividend < 0 and divisor < 0:
+            dividend = abs(dividend)
+            divisor = abs(divisor)
+            r = 1
             
-        else:
-            result = ''
-        c = 0
-        for i in range (dividend,-1,-divisor):
-            if i >= divisor:
-                c+=1
-        
-        return result + str(c)
+        elif dividend < 0 and divisor > 0:
+            dividend = abs(dividend)
+            r= -1
+        elif dividend >= 0 and divisor < 0:
+            divisor = abs(divisor)
+            r=-1
+        elif dividend >= 0 and divisor > 0:
+            r=1
+        elif dividend < divisor:
+            return div
+        if divisor == 1:
+            div = dividend
+           
+        while dividend >= divisor and divisor != 1 and divisor != 0:
+            dividend -= divisor
+            div +=1
+        return r*div
 
-print( divide( -1, -1))
+
+print( divide( -2147483648,-1))
--- Day14/insertionSort.py
+++ Day14/insertionSort.py
@@ -2,13 +2,14 @@
 #insertion sort
 
 def insertionSort(n, nums):
-    for i in range(1, len(nums)):
+    for i in range(1, n):
         current_val = nums[i]
         j = i- 1
         while j >=0 and current_val < nums[j]:
             nums[j+1] = nums[j]
             j-=1
         nums[j+1]=current_val
+        
     return nums
     
         
