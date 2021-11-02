3eeff7754381ec40c8e0e38c5e0564f0487845b5
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-11-02 15:44:14 +0300

    binary number

--- Day18/binaryNumber.py
+++ Day18/binaryNumber.py
@@ -4,15 +4,28 @@ def bunaryNum(num):
     
     l=''
     c = 0
+    x=0
+    h=''
     while num > 0:
         rem = num % 2
         num = num//2 
         l+=str(rem)
-    s = ''
-    l=l[::-1]
-    while l[c] !='0' and c<len(l):
-        s+=l[c]
-        c+=1
-   
-    return c
-print(bunaryNum(125))
+    print(l)
+    if '0'  in l:
+        while l[x] !='0' and x<len(l):
+            h+=l[x]
+            x+=1
+    
+        s = ''
+        l=l[::-1]
+        while l[c] !='0' and c<len(l):
+            s+=l[c]
+            c+=1
+    else:
+        c=len(l)
+    if (x > c):
+        return x
+    else:
+        
+        return c
+print(bunaryNum(5))
