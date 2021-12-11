dba0632e82c6dac7a869afbabef43eb0330d18c8
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-12-11 10:35:02 +0300

    sorting the sentence

--- a2svDay3/sort_sentence.py
+++ a2svDay3/sort_sentence.py
@@ -1,15 +1,15 @@
 #leetcode
 def sortingsent(s):
     s1 = s.split(' ')
-    s2=[]
+    r=''
+    Dict = {}
     for i in s1:
-        for j  in s1:
-            if i[-1] < j[-1]:
-                # s1[i] , s1[j] = s1[j], s1[i]
-                print(i)
-                s2.append(i)
-                # c = s1.index(i)
-                # s1.remove(cd)
-    print(s2)
-    return s1
+        Dict[i[-1]] = i[:len(i)-1]
+        #this will make the numbers key and the string value
+    for i in range(1,len(s1)+1):
+        r += Dict[str(i)] + ' '
+        # i as a key to our dictionary then sort and put it to a string
+        
+    return r.strip()
+
 print(sortingsent("is2 sentence4 This1 a3"))
