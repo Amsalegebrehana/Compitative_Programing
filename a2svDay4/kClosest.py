1d8f906731d790d96b92b9120572dc8ec1865106
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-12-11 19:33:53 +0300

    kClosest

--- a2svDay4/kClosest.py
+++ a2svDay4/kClosest.py
@@ -19,10 +19,11 @@ def kClosest(points,k):
             Dict[l[i]].append(points[i])
        
     
-    for i in range(0, len(l)):
-        for j in range(0, len(l)):
-            if l[i] < l [j]:
-                l[i],l[j] = l[j],l[i]
+    # for i in range(0, len(l)):
+    #     for j in range(0, len(l)):
+    #         if l[i] < l [j]:
+    #             l[i],l[j] = l[j],l[i]
+    l.sort()
     print(l)
     i = 0
     count= 0
