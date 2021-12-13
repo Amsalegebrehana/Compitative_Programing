c99cae3067546fddb95dc0c1ccf98f40cfb1e552
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-12-13 12:51:18 +0300

    merge intervals

---
+++ a2svDay4/Merge Intervals.py
@@ -0,0 +1,30 @@
+
+
+def mergeIntervals(intervals ):
+    intervals = sorted(intervals)  
+    i=0
+    while(i<len(intervals)-1):
+
+
+        if  intervals[i][1] >= intervals[i+1][0]:
+            
+            if intervals[i+1][1] > intervals[i][1]:
+                
+                k = intervals[i+1][1]
+            else:
+                k= intervals[i][1]
+            x = [intervals[i][0],k ]
+            intervals.pop(i+1)
+            intervals.pop(i)
+            intervals.insert(i,x)
+        else:
+
+            i+=1
+        # print(l)
+
+
+     
+    return intervals
+print(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]))
+                
+    
---
+++ a2svDay4/largestNumber.py
@@ -0,0 +1,9 @@
+
+
+def largestNum(nums):
+    l = []
+    for i in nums:
+        s = ''.join(nums)
+        # l.append(s)
+    return s
+print(largestNum([10,2]))
