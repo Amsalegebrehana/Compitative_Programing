c5eec80aa742dbcc0a0a8141e7f623cf6f427984
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-12-14 09:34:49 +0300

    rearrange array

--- a2svDay5/rearrangeArray.py
+++ a2svDay5/rearrangeArray.py
@@ -1,10 +1,9 @@
 
 
 def rearrangeArray(nums):
-    l=[]
+ 
     nums[:]=sorted(nums)
-    l=[]
-    nums[:]=sorted(nums)
+   
     for i in range(0, len(nums)-1):
         if i%2==0:
             nums[i], nums[i+1] = nums[i+1],nums[i]
