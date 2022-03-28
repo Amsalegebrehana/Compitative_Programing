a41a312d02fb3c1ad5c1105a7f3c89f93f003c8e
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2022-03-29 00:47:06 +0300

    2170. Minimum Operations to Make the Array Alternating

--- dailyQuestions/2170. Minimum Operations to Make the Array Alternating.py
+++ dailyQuestions/2170. Minimum Operations to Make the Array Alternating.py
@@ -1,4 +1,5 @@
 # 2170. Minimum Operations to Make the Array Alternating
+from typing import Counter
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         if len(nums) == 1:
@@ -9,10 +10,7 @@ class Solution:
         sizeeven = len(nums)//2 if len(nums)%2 ==0 else (len(nums)//2) +1
        
         if even[0][0] != odd[0][0]:
-            le = sizeeven - even[0][1]
-           
-            lo = sizeodd - odd[0][1]
-            return lo + le
+            return (sizeeven - even[0][1]) + (sizeodd - odd[0][1])
         else:
             
             if len(odd)==len(even)==1:
@@ -21,13 +19,7 @@ class Solution:
                 minn = min((sizeeven - even[0][1] + sizeodd - odd[1][1]),(sizeeven - even[1][1] + sizeodd - odd[0][1]))
                 return minn
             elif len(odd) == 1 and len(even) > 1:
-                le = sizeeven - even[1][1]
-              
-                lo = sizeodd - odd[0][1]
-                return lo + le
+                return (sizeodd - odd[0][1]) + (sizeeven - even[1][1])
             elif len(odd) > 1 and len(even) == 1:
-                le = sizeeven - even[0][1]
-              
-                lo = sizeodd - odd[1][1]
-                return lo + le
+                return (sizeodd - odd[1][1]) + (sizeeven - even[0][1])
         
