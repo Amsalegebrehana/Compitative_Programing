7632b14f36056dfd692b362b48a427b2d6c7cdf0
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-09-16 23:15:40 +0300

    Time: 2402 ms (5.02%), Space: 69.8 MB (6.28%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,21 +1,20 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        incoming = defaultdict(list)
-        outgoing = defaultdict(list)
-        v = set()
-        for i,j in edges:
-            v.add(i)
-            v.add(j)
-            incoming[i].append(j)
-            outgoing[j].append(i)
+        graph = defaultdict(set)
 
-        m= max(v)
-        ans = []
-        for i in range(1,m+1):
-            ans.append((i,len(incoming[i])+len(outgoing[i])))
-        heap = []
-        for i in ans:
-            heappush(heap,(-i[1],i[0]))
+        for i,j in edges:
+        
+            graph[i].add(j)
+            graph[j].add(i)
+        
+        maxval = 0
+        star = 0
+
+        for key, val in graph.items():
+            if len(val) > maxval:
+                maxval = len(val)
+                star = key
+
       
-        return heappop(heap)[1]
+        return star
             
