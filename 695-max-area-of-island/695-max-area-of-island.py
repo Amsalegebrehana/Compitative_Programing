aad752e443294fe5527cb71a427771d16cd29261
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-09-21 11:20:17 +0300

    Time: 160 ms (85.44%), Space: 17.7 MB (27.16%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -6,29 +6,23 @@ class Solution:
             if 0<= r < len(grid) and 0<= c < len(grid[0]):
                 return True
             return False
-        def bfs(r,c):
-            queue = deque([[r,c]])
+        def dfs(r,c):
             visited.add((r,c)) 
-            c = 1
-            while queue:
-                curr = queue.popleft()
-               
-                for i, j in dir_arr:
-                    nr,nc = curr[0] + i, curr[1] + j
-                    if bound(nr,nc) and grid[nr][nc] == 1 and (nr,nc) not in visited:
-                        visited.add((nr,nc))
-                        queue.append([nr,nc])
-                        c+=1
-            return c
+            ct = 1  
+            for i, j in dir_arr:
+                nr,nc = r + i, c + j
+                if bound(nr,nc) and grid[nr][nc] == 1 and (nr,nc) not in visited:
+                    visited.add((nr,nc))
+                    ct += dfs(nr,nc)
+            return ct
                         
         maxcount = 0
 
         for i in range(len(grid)):
             
             for j in range(len(grid[0])):
-                if grid[i][j] == 1 :
-               
-                    maxcount = max(maxcount, bfs(i,j))
+                if grid[i][j] == 1 and (i,j) not in visited :
+                    maxcount = max(maxcount, dfs(i,j))
         return maxcount
 
             
