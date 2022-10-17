0d99372d162a0d30b30e73014172aeb9916cb242
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-10-17 15:34:30 +0300

    Time: 212 ms, Memory: 43.2 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,13 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        l = 0
-        r = len(s) -1        
-        def recursion(l,r):
-            if l < r:
-                s[l],s[r]=s[r],s[l]
-                recursion(l+1, r-1)
+        def rec(s,l,r):
+            if l <= r:
+             
+                s[l], s[r] = s[r], s[l]
+                return rec(s, l + 1, r-1)
+        rec(s,0,len(s) -1)
+               
         
-        recursion(l,r)
-            
-            
