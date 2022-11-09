9a25c97ed66702fc57658faafcf51e3a3c4f6040
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-11-09 17:37:37 +0300

    Time: 217 ms (87.03%), Space: 15.8 MB (41.47%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -2,15 +2,13 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         numbers_freq = defaultdict(int)
         for i in nums:
-            
             numbers_freq[i]+=1
         
-        res = []
+        missing_num = -1
+        dupulicate_num = -1
         for i in range(1,len(nums)+1):
             if numbers_freq[i]>1:
-                res.append(i)
-            
-        for i in range(1,len(nums)+1):
-            if  numbers_freq[i] == 0 :
-                res.append(i)
-        return res
+                dupulicate_num = i
+            elif  numbers_freq[i] == 0 :
+                missing_num=i
+        return [dupulicate_num,missing_num]
