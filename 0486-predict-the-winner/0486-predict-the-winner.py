99a22b77fadd65e04e0f4564e070966e560498c1
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-11-09 13:51:46 +0300

    Time: 77 ms (33.59%), Space: 14.2 MB (17.49%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,6 +1,6 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        memo = {}
+        @cache
         def dp(i,  j):
             ch1 = 0
             ch2 = 0
@@ -8,12 +8,11 @@ class Solution:
                 return 0
            
             else:
-                if (i,  j) not in memo:
-                    ch1 = nums[i] + min(dp(i+1, j-1), dp(i+2, j))
-
-                    ch2 = nums[j] +  min(dp(i+1,  j-1), dp(i,  j-2))
-                    memo[(i,  j)]= max(ch1, ch2)
-            return memo[(i,  j)]
+                
+                ch1 = nums[i] + min(dp(i+1, j-1), dp(i+2, j))
+             
+                ch2 = nums[j] +  min(dp(i+1,  j-1), dp(i,  j-2))
+            return max(ch1, ch2)
         p1 = dp(0, len(nums) - 1)
  
         return p1>= sum(nums)-p1
