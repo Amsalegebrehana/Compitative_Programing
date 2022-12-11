391ec2f3d86ec019308d2c49e69f09b29f89d0ff
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-12-11 15:35:21 +0300

    Time: 733 ms (83.31%), Space: 15.8 MB (19.26%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -8,21 +8,24 @@ class Solution:
         for r in range(M):
             for c in range(M):
                 if grid[r][c] == 1:
-                    queue.append((r,c,0))
-                  
+                    queue.append((r,c))
+                    visited.add((r,c))
 
         def bound(r,c):
             return M > r >=0 and M > c >= 0
         while queue:
-           
-            curr = queue.popleft()
-   
-            for r,c in dir_arr:
-                nr,nc = r + curr[0], c + curr[1]
-                if bound(nr,nc) and grid[nr][nc] == 0 :
-                    dist = max(dist,curr[2] + 1)
-                    queue.append((nr,nc, curr[2] + 1))
-                    grid[nr][nc] = 1
-      
+            size = len(queue)
+            for _ in range(size):
+                curr = queue.popleft()
+     
+                for r,c in dir_arr:
+                    nr,nc = r + curr[0], c + curr[1]
+                    if bound(nr,nc) and grid[nr][nc] == 0 and (nr,nc) not in visited:
+                        
+                        queue.append((nr,nc))
+                        visited.add((nr,nc))
+                        
+            if queue:
+                dist +=1
         return dist if dist != 0 else -1
         
