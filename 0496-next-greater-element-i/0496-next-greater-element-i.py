3422714159168b6e2282925fa73c4827ee3b279d
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-12-29 10:43:45 +0300

    Time: 46 ms (96.23%), Space: 14.1 MB (56.12%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,20 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n = len(nums2)
-        st = []
-        result = [-1] * n
-        for i in range(n-1, -1, -1):
-            while st and st[-1] < nums2[i]:
-                st.pop()
-            if st:
-                result[i] = st[-1]
-            st.append(nums2[i])
-
-        dictt = {}
-        for i in range(n):
-            dictt[nums2[i]] = result[i]
-    
-        ans = []
-        for i in nums1:
-            ans.append(dictt[i])
-        return ans
+        stack = []
+        mapp = {}
+        
+        for num in nums2:
+            
+            while(stack and num > stack[-1]):
+                c = stack.pop()
+                mapp[c] = num
+            
+            stack.append(num)
+  
+        return [mapp.get(num, -1)  for num in nums1 ]
