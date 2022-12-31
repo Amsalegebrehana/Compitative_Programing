ad4dc2636fbefcce9028f88691f0510ac06777b1
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2022-12-31 16:26:02 +0300

    Time: 1010 ms (92.61%), Space: 25 MB (36.26%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -4,21 +4,18 @@ class Solution:
         idx = 0
         n = len(nums)
         mindiff = inf
-        flag = False
+
         for i in range(1,len(nums)):
             presum.append(presum[-1] + nums[i])
 
-        for i in range(len(presum)-1):
- 
-            diff = abs((presum[i]//(i+1)) - ((presum[-1]- presum[i])//(n - (i+1))))
+        for i in range(len(presum)):
+            div = n - (i+1)
+            if n - (i+1) == 0:
+                div = n 
+            
+            diff = abs((presum[i]//(i+1)) - ((presum[-1]- presum[i])//(div)))
             if diff < mindiff:
                 mindiff = diff
                 idx = i
-                flag = True
-        if flag:
-            i+=1
-            diff = abs((presum[i]//(i+1)) - ((presum[-1]- presum[i])//n ))
-            if diff < mindiff:
-                mindiff = diff
-                idx = i
+     
         return idx
