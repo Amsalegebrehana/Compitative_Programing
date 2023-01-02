c335acb51965c57de64fd2173dc948318e6ce0d0
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-01-02 17:55:11 +0300

    Time: 1839 ms (90.05%), Space: 60.5 MB (86.18%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next == None:
+            return None
+        slow = head
+        prev = slow
+        fast = head
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev.next = slow.next
+      
+        return head
