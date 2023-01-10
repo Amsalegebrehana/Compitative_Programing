d01ca33027e15ce1b7460080a95acc44678427ad
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-01-10 10:21:58 +0300

    Time: 78 ms (86.38%), Space: 13.8 MB (99.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,20 +1,22 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        dir_arr = [(1,0),(0,1),(-1,0),(0,-1)]
-        queue = deque([(sr,sc)])
-        visited = {(sr,sc)}
-        
-        start = image[sr][sc]
+        dir_arr  = [(1,0),(0,1),(-1,0),(0,-1)]
+        org = image[sr][sc]
         image[sr][sc] = color
-        
+        queue = deque([(sr,sc)])
+        visited = set((sr,sc))
+        M = len(image)
+        N = len(image[0])
         def bound(r,c):
-            return len(image) > r >= 0 and len(image[0]) > c >= 0
+            return M > r>= 0 and N > c >= 0
         while queue:
-            curr = queue.popleft()
-            for r,c in dir_arr:
-                nr,nc = r + curr[0], c + curr[1]
-                if bound(nr,nc) and image[nr][nc] == start and (nr,nc) not in visited:
-                    visited.add((nr,nc))
-                    queue.append((nr,nc))
-                    image[nr][nc] = color
+            row,col = queue.popleft()
+            for r, c in dir_arr:
+                newrow, newcol = row + r, col + c
+                if bound(newrow,newcol) and image[newrow][newcol] == org and (newrow, newcol) not in visited:
+                    
+                    image[newrow][newcol] = color
+                    
+                    queue.append((newrow, newcol))
+                    visited.add((newrow, newcol))
         return image
