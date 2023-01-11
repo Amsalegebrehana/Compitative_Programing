63ab391572f3e7f6a8e439d578aa5c98c1b31e73
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-01-11 14:57:45 +0300

    Time: 243 ms (69.73%), Space: 17.3 MB (25.69%) - LeetHub

--- 0820-short-encoding-of-words/0820-short-encoding-of-words.py
+++ 0820-short-encoding-of-words/0820-short-encoding-of-words.py
@@ -1,10 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, word):
+        length = 0
+        currNode = self.root
+        for ch in word:
+            if ch not in currNode.children:
+                currNode.children[ch] = TrieNode()
+            currNode = currNode.children[ch]
+            length+=1
+        currNode.end = True
+        return length+1
+    def startWith(self, word):
+        currNode = self.root
+        for ch in word:
+            if ch not in currNode.children:
+                return False
+            currNode = currNode.children[ch]
+        return True
+        
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        w = set(words)
-        for word in words:
-            for j in range(1,len(word)):
-                if word[j:] in w:
-                    w.remove(word[j:])
+        words.sort(key=lambda x : len(x), reverse= True)
 
-        return len("#".join(w)) + 1
-                    
+        trie = Trie()
+        encodelen = 0
+        for word in words:
+            word_reversed = word[::-1]
+         
+            if not trie.startWith(word_reversed):
+                encodelen += trie.insert(word_reversed)
+        return encodelen
+        
