c3d20d6bdab50c69cb0142149fd5eb789b823f60
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-01-16 05:51:20 +0300

    Time: 93 ms (55.29%), Space: 17.3 MB (52.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,8 +1,9 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        intervals.append(newInterval)
-        intervals.sort()
+        position = bisect.bisect(intervals, newInterval)
+        # O(N)
+        intervals.insert(position, newInterval)
         result = [intervals[0]]
         for i in range(1,len(intervals)):
             if intervals[i][0] <= result[-1][1]:
