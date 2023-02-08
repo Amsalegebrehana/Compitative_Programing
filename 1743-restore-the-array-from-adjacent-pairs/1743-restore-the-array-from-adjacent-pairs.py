43caad232d71aa9f45cf62bc35139f23c88b3da0
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-02-08 16:58:54 +0300

    Time: 1192 ms (66.19%), Space: 77.2 MB (29.80%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -1,18 +1,16 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
         graph = defaultdict(set)
-        nebours = defaultdict(int)
         visited = set()
         result = []
         queue = deque([])
+        
         for i , j in adjacentPairs:
             graph[i].add(j)
             graph[j].add(i)
-            nebours[i] +=1
-            nebours[j] +=1
 
-        for num in nebours:
-            if nebours[num] == 1:
+        for num in graph:
+            if len(graph[num] )== 1:
                 start = num
                 visited.add(num)
                 
