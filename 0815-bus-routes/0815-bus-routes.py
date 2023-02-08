399bfe288ef34055d4500cf5e8737422a12b584d
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-02-08 23:11:24 +0300

    Time: 503 ms (61.09%), Space: 56.8 MB (6.13%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+        graph = collections.defaultdict(set)
+        
+        for i in range(len(routes)):
+            for j in routes[i]:
+                graph[j].add(i)
+                
+        targets = [(source, 0)]
+        visited = set([source])
+        
+        for stop, bus in targets:
+            if stop == target: 
+                return bus
+            for i in graph[stop]:
+                for j in routes[i]:
+                    if j not in visited:
+                        targets.append((j, bus + 1))
+                        visited.add(j)
+                routes[i] = []  
+        return -1
+        
