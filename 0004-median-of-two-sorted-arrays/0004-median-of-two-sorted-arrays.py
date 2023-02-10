cacbb52d122b1a5c5b50ed15717b4bbe76e24510
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-02-10 22:02:21 +0300

    Time: 89 ms (90.32%), Space: 14.2 MB (18.24%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,9 +3,10 @@ class Solution:
         nums = nums1 + nums2
         nums.sort()
   
-        if len(nums) % 2 != 0:
-            return nums[len(nums) // 2]
-        else:
-           
+        if len(nums) % 2 == 0:
             return (nums[len(nums) // 2] + nums[(len(nums) // 2)  - 1]) / 2
+            
+        else:
+            return nums[len(nums) // 2]
+            
             
