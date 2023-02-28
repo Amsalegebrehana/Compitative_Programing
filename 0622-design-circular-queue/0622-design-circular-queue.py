fe0725b6476d7452100616d24c0b081b4d32ce60
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-02-28 23:21:12 +0300

    Time: 72 ms (61.12%), Space: 14.5 MB (50.73%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -0,0 +1,51 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.arr = [0]*k
+        self.rear = 0
+        self.front = 0
+        self.length = 0
+
+    def enQueue(self, value: int) -> bool:
+        
+        if self.isFull():
+            return False
+
+        self.arr[self.rear] = value
+        self.rear = (self.rear + 1)%len(self.arr)
+        self.length += 1
+
+        return True
+    def deQueue(self) -> bool:
+        
+        if self.isEmpty():
+            return False
+
+        self.front = (self.front + 1)%len(self.arr)
+        self.length -= 1
+
+        return True
+
+
+    def Front(self) -> int:
+        
+        return self.arr[self.front] if not self.isEmpty() else -1
+    
+    def Rear(self) -> int:
+        return self.arr[self.rear-1] if not self.isEmpty() else -1
+
+    def isEmpty(self) -> bool:
+        return self.length == 0
+
+    def isFull(self) -> bool:
+        return self.length == len(self.arr)
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
