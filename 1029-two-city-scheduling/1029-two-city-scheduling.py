643af217f4182be2c1ef328ae3a258e806116f4b
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-04-03 21:44:34 +0300

    Time: 41 ms (66.53%), Space: 13.8 MB (65.34%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        sumpplCityA = 0
-        diffAB = []
-        size = len(costs)
-
-        for ca, cb in costs:
-            sumpplCityA += ca
-            diffAB.append(cb - ca)
-
-        diffAB.sort()
-
-        result = sumpplCityA +  sum(diffAB[:size // 2]) 
-
-        return result
+        
+        totalCost = 0
+        diff = []
+        
+        for a,b in costs:
+            diff.append(b - a)
+            totalCost += a
+        
+        diff.sort()
+      
+        return totalCost + sum(diff[:len(diff)//2])
+        
