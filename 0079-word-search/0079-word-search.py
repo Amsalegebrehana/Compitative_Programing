cd2c594a0728cbe4fc71953d91c608d7ea677cd5
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-04-05 08:22:54 +0300

    Time: 8316 ms (34.59%), Space: 13.9 MB (43.65%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        dir_arr = [(0,-1),(-1,0),(1,0),(0,1)]
-        strings = set()
-        visited = set()
-        M,N =  len(board), len(board[0])
-        def bound(r,c):
-            return  M > r >= 0 and N > c >= 0
-              
-        def backtrack(start,i):
-            if i == len(word) :
-                return True
-            val = False
-            for r,c in dir_arr:
-                nr,nc = r + start[0], c + start[1]
-                if bound(nr,nc) and (nr,nc) not in visited and board[nr][nc] == word[i]:
-                    visited.add((nr,nc))
-                    val =  backtrack((nr,nc), i + 1)
-                    if val:
-                        return True
-                    visited.remove((nr,nc))
+            dir_arr = [(1,0),(0,1),(-1,0),(0,-1)]
+            M = len(board)
+            N = len(board[0])
+            def bound(r,c):
+                return M > r >= 0 and N > c >= 0
+            def backtrack(pathword, r,c):
+                if "".join(pathword) == word:
+                    return True
+
+                for i, j  in dir_arr:
+                   
+                    newr, newc = r + i, c + j
+                    if bound(newr,newc) and board[newr][newc] == word[len(pathword)]:
+                        pathword.append(board[newr][newc])
+                        board[newr][newc] = "#"
+                        if backtrack(pathword, newr,newc):
+                            return True
+                        
+                        board[newr][newc] = pathword.pop()
+                return False
+            for r in range(M):
+                for c in range(N):
+                    if board[r][c] == word[0]:
+                        pathword = [board[r][c]]
+                        board[r][c] = "#"
+                        if backtrack(pathword,r,c):
+                            return True
+                        board[r][c] = pathword.pop()
             return False
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    visited.add((i,j))
-                    var = backtrack((i,j), 1)
-                    if var:
-                        return True
-                    visited.remove((i,j))
-              
-               
-        return False
+            
