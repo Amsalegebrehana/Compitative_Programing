6a7536f3199b88ba036c0942d6bf589f5a6ea94b
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-04-15 11:34:19 +0300

    Time: 213 ms (64.75%), Space: 14.7 MB (38.21%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,8 +1,8 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.stream = defaultdict(str)
-        self.n = n + 1
+        self.stream = [None] * (n + 1)
+        
         self.ptr = 1
 
     def insert(self, idKey: int, value: str) -> List[str]:
@@ -11,7 +11,7 @@ class OrderedStream:
         
         if  self.ptr == idKey:
             chunk = []
-            while self.ptr < self.n and len(self.stream[self.ptr]) != 0:
+            while self.ptr < len(self.stream) and self.stream[self.ptr] != None:
                
                 chunk.append(self.stream[self.ptr])
                 self.ptr +=1
