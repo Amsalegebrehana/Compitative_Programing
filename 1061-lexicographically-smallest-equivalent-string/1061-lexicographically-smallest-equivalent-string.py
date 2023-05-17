9a46c0c23a030411cddb84753ee9c6edbd8536be
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-05-17 21:41:35 +0300

    Time: 49 ms (51.03%), Space: 16.4 MB (11.49%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,46 +1,32 @@
-class UnionFind:
-    def __init__(self):
-        self.parent = [i for i in range(26)]
-        
-    def find(self, x):
-        
-        while x != self.parent[x]:
-            self.parent[x] = self.parent[self.parent[x]]
-            x = self.parent[x]
-            
-        return x
-    
-    def union(self, x, y):
-        
-        px = self.find(x)
-        py = self.find(y)
-        
-        if px != py:
-            px,py = sorted([px,py])
-            self.parent[py] = self.parent[px]
-        
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
         
-        val_a = ord("a")
-        unionfind = UnionFind()
-        minorders = []
+        letters = {chr(i):chr(i) for i in range(97,123)}
+       
+        
+        def find(ch):
+            if letters[ch] == ch:
+                return ch
+            
+            letters[ch] = find(letters[ch])
+            return letters[ch]
+        
+        def union(a,b):
+            
+            pa = find(a)
+            pb = find(b)
+         
+            if pa != pb:
+                if pa < pb:
+                  
+                    letters[pb] = pa
+                else:
+                    letters[pa] = pb
+                
         for i in range(len(s1)):
-            unionfind.union(ord(s1[i])- val_a, ord(s2[i])- val_a)
-            
-        for i in range(len(baseStr)):
-            num_val_i = ord(baseStr[i]) - val_a
-            smallest = chr(val_a + (unionfind.find(num_val_i)))
-            minorders.append(smallest)
+            union(s1[i],s2[i])
 
-        return "".join(minorders)
-        
-        
-            
-            
-            
-            
-            
-            
-            
-        
+        ans = ""
+        for ch in baseStr:
+            ans += find(letters[ch])
+        return ans
